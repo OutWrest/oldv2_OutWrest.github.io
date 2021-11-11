75d2d4f026bb3786fcc4f05c82cf1fe7e12e23e1
Salah <0utwrest@protonmail.com>
2021-11-10 23:27:26 -0600

    Basic changes to ui + removed basic react

---
+++ src/title/Title.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import './Title.css'
+
+function Title() {
+    return (
+        <Container>
+            <Container fluid>
+                <h1>Salah Abbas</h1>
+            </Container>
+            <h1>Hello World</h1>
+        </Container>
+    );
+}
+
+export default Title;
